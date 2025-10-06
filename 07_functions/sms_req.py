2115a24bc3e0b31beb5928c0edab323c42765db5
raadhajayanthi <raadhajayanthi@gmail.com>
2025-10-06 21:08:23 +0530

    completed using function lms

--- 07_functions/sms_req.py
+++ 07_functions/sms_req.py
@@ -13,7 +13,7 @@ def display_system_info():
   print(f"    Developed By Students at: ", SYSTEM_INFO[3])
   print("="*50)
 
-display_system_info()
+
 
 
 #student info is dictionary
@@ -27,7 +27,8 @@ def show_menu():
     print("2 = Modify Student")
     print("3 = Delete Student")
     print("4 = List Student")
-    print("5 = Exit Student")
+    print("5 = Student skill search ")
+    print("6 = Exit Student")
 
 
 
@@ -123,6 +124,18 @@ def listing_student(students):
                 print(f"All skills:{all_skills}")
                 print(f"skills count:{count_skills}")
 
+
+def search_student_by_skill(students):
+      student_skill = input("enter student skill: ").title()
+      matched_student = list(filter(lambda s: student_skill in students[s]['skills'] , students))
+      if matched_student:
+                print(f"Students With Skills {student_skill} :")
+                for sid in matched_student:
+                 print(f"Id: {sid}, Name: {students[sid]["name"]}")
+      else:
+        print(f"no student found with skill {student_skill} ")                                                                                                   
+
+
 def existing_system(students):
         print("Exiting System")   
         print("="*50)
@@ -130,10 +143,15 @@ def existing_system(students):
         print("="*50)
         print(f"ADMIN PHONE: {ADMIN_INFO[1]}")
         print(f"ADMIN EMAIL: {ADMIN_INFO[0]}")
-    
+
+
+
+display_system_info()
+
+
 while True:
     show_menu()
-    choice = input("Enter Your Choice(1-5): ")
+    choice = input("Enter Your Choice(1-6): ")
     
     if choice == "1":
         add_student(students) 
@@ -144,6 +162,8 @@ while True:
     elif choice =="4":
          listing_student(students)
     elif choice =="5":
+         search_student_by_skill(students)    
+    elif choice =="6":
          existing_system(students)
          break
     else:
